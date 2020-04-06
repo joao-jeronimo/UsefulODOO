175856225e6cef97dae32eb1515b8fe67d74de35
João Jerónimo <joao.jeronimo.pro@gmail.com>
2020-04-06 21:47:41 +0100

    Melhor.

--- AppStats/get_all_apps.bash
+++ AppStats/get_all_apps.bash
@@ -12,3 +12,20 @@ echo "Total `wc -l all_apps_urls.txt` apps"
 # Prepend URL start:
 sed -e "s/^/https:\/\/apps.odoo.com/" all_apps_urls.txt > trash/tmp5.txt
 mv trash/tmp5.txt all_apps_urls.txt
+
+# Getting all modules pages:
+for modurl in `cat all_apps_urls.txt`
+do
+    echo wget "$modurl" -O trash/mod_named_"`printf '%s' "$modurl" | grep -o '[^/]\+$'`"_page.html
+done > trash/modpage_getting_script.bash
+
+# Separate in batches for parallel exec:
+sed -n -e "0~5p" trash/modpage_getting_script.bash > trash/modpage_getting_script_batch0.bash
+sed -n -e "1~5p" trash/modpage_getting_script.bash > trash/modpage_getting_script_batch1.bash
+sed -n -e "2~5p" trash/modpage_getting_script.bash > trash/modpage_getting_script_batch2.bash
+sed -n -e "3~5p" trash/modpage_getting_script.bash > trash/modpage_getting_script_batch3.bash
+sed -n -e "4~5p" trash/modpage_getting_script.bash > trash/modpage_getting_script_batch4.bash
+
+#bash trash/modpage_getting_script.bash
+#grep '<td><b>License</b></td>' trash/mod_named_dev_payslip_batches_page.html
+
