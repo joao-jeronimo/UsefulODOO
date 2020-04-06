f9d86002b57654f27d6c2035a0ab8cca06b30de5
João Jerónimo <joao.jeronimo.pro@gmail.com>
2020-04-06 21:13:59 +0100

    So URLs modulos.

--- AppStats/get_all_apps.bash
+++ AppStats/get_all_apps.bash
@@ -1,9 +1,11 @@
+mkdir -f trash
+
 for i in {1..520}
   do
   #echo $i
-  wget "https://apps.odoo.com/apps/modules/browse/page/$i?price=Paid" -O Page"$i".html
+  wget "https://apps.odoo.com/apps/modules/browse/page/$i?price=Paid" -O trash/Page"$i".html
 done
 
-
-cat Page*.html | grep -o 'href="/apps/modules/[0-9.]\+/[^"]\+"' | tee all_apps_urls.txt
+cat trash/Page*.html | grep -o 'href="/apps/modules/[0-9.]\+/[^"]\+"' | grep -o '/[^"]\+' | sort -u | tee all_apps_urls.txt
+echo "Total `wc -l all_apps_urls.txt` apps"
 
