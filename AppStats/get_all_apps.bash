22dd413d4950e9698bcf7737869ed578e2c10de0
João Jerónimo <joao.jeronimo.pro@gmail.com>
2020-04-06 21:00:58 +0100

    Script para sacar os nomes de todas as apps do odoo.

---
+++ AppStats/get_all_apps.bash
@@ -0,0 +1,9 @@
+for i in {1..520}
+  do
+  #echo $i
+  wget "https://apps.odoo.com/apps/modules/browse/page/$i?price=Paid" -O Page"$i".html
+done
+
+
+cat Page*.html | grep -o 'href="/apps/modules/[0-9.]\+/[^"]\+"'
+
