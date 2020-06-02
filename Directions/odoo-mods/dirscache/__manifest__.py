41eae72b10043345ce12a3f8c171b1f0f9f89084
João Jerónimo <joao.jeronimo.pro@gmail.com>
2020-06-02 23:07:34 +0100

    Directions Odoo.

---
+++ Directions/odoo-mods/dirscache/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name':'Caching directions server for Odoo',
+    #'description':'Latest Posts Snippet',
+    'category': '',
+    'version':'0.1',
+    'author':'João Jerónimo',
+    'data': [
+        'views/test_view.xml',
+        'views/menus.xml',
+    ],
+    'depends': [],
+    'auto_install': False,
+    #'images': [
+    #    'static/description/icon.png',
+    #],
+}
---
+++ Directions/test-python/testes.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+api_key = "5b3ce3597851110001cf6248209f07136a7a44c0be17479e56a02c78"
+
+theURL = "https://api.openrouteservice.org/v2/directions/driving-car?api_key=5b3ce3597851110001cf6248209f07136a7a44c0be17479e56a02c78&start=8.681495,49.41461&end=8.687872,49.420318"
+
