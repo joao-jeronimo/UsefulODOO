b0d70e54ebfd8b500703e9483b0d610738e81b01
João Jerónimo <joao.jeronimo.pro@gmail.com>
2020-07-12 16:26:55 +0100

    Nova solução - baseada num módulo Odoo.

---
+++ ODOO-config-scripts/v3 - mod/shell_executor/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name':'Execute shell commands',
+    #'description':'Latest Posts Snippet',
+    'category': '',
+    'version':'0.1',
+    'author':'João Jerónimo',
+    'data': [
+        'views/command_views.xml',
+        'views/menus.xml',
+    ],
+    'depends': [],
+    'auto_install': False,
+    #'images': [
+    #    'static/description/icon.png',
+    #],
+}
