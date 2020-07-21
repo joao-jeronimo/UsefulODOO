fe88a00c18db2e5e24159d9a0f15691246df8859
Joao Jeronimo <joao.jeronimo.pro@gmail.com>
2020-07-21 23:28:53 +0000

    Finalmente consegue instalar...

--- ODOO-config-scripts/v3 - mod/scripts/install.bash
+++ ODOO-config-scripts/v3 - mod/scripts/install.bash
@@ -33,7 +33,7 @@ mkdir "$ODOOMAN_DIR"/logs/
 mkdir "$RELEASES_DIR"/
 
 # Generate and prepare a password:
-export ODOO_PASSWORD=`pwgen -y -c -n 40 1 --secure | tr -d "\n"`
+export ODOO_PASSWORD=`pwgen -c -n 40 1 --secure | tr -d "\n"`
 echo "{'DB_PASSWORD': '$ODOO_PASSWORD'}" > "$SCRIPTCONFIG"
 
 # Install dependencies:
@@ -46,7 +46,7 @@ apt build-dep -y python3-ldap
 
 sudo -H pip3 install --upgrade pip
 sudo -H pip3 install --upgrade six pillow python-dateutil pytz
-sudo -H pip3 install --ignore-installed pyserial
+sudo -H pip3 install --ignore-installed pyserial psycopg2-binary
 
 sudo -H pip3 install xlrd xlwt pyldap qrcode vobject num2words phonenumbers
 
@@ -62,10 +62,94 @@ sudo useradd -d "$ODOOMAN_DIR"/ "$ODOO_USERNAME"
 ######################################
 # Clone the 13 version from the main tree:
 git clone --single-branch -b "$ODOOMAN_ODOO_REL" "$MAIN_GIT_LOCAL_REPO" "$RELEASES_DIR"/"$ODOOMAN_ODOO_REL"
+# Install version-specific deps:
+cd "$RELEASES_DIR"/"$ODOOMAN_ODOO_REL"
+sudo -H pip3 install -r requirements.txt
+
 # Install the config file:
-# TODO!
+cat << EOF > "$ODOOMAN_DIR"/configs/odooman.conf
+[options]
+addons_path = $RELEASES_DIR/$ODOOMAN_ODOO_REL/addons
+#admin_passwd =
+csv_internal_sep = ,
+data_dir = /odoo/.local/share/Odoo
+# Use unix sockets with no password:
+#db_host = localhost
+#db_port = 5432
+#db_password = $ODOO_PASSWORD
+db_maxconn = 64
+db_name = odooman
+db_sslmode = prefer
+db_template = template1
+db_user = $ODOO_USERNAME
+demo = {}
+email_from = False
+;geoip_database = /usr/share/GeoIP/GeoLite2-City.mmdb
+http_enable = True
+http_interface = 0.0.0.0
+http_port = 8000
+import_partial = 
+limit_memory_hard = 2684354560
+limit_memory_soft = 2147483648
+limit_request = 8192
+limit_time_cpu = 60
+limit_time_real = 120
+limit_time_real_cron = -1
+list_db = True
+log_db = False
+log_db_level = warning
+log_handler = :INFO
+log_level = info
+logrotate = False
+longpolling_port = 8072
+max_cron_threads = 2
+osv_memory_age_limit = 1.0
+osv_memory_count_limit = False
+pg_path = None
+pidfile = False
+proxy_mode = True
+reportgz = False
+server_wide_modules = web
+smtp_password = False
+smtp_port = 25
+smtp_server = localhost
+smtp_ssl = False
+smtp_user = False
+syslog = False
+test_commit = False
+test_enable = False
+test_file = False
+test_report_directory = False
+translate_modules = ['all']
+unaccent = False
+without_demo = True
+workers = 0
+EOF
+
 # Install the systemd script:
-# TODO!
+cat << EOF > "/lib/systemd/system/odoomanager.service"
+[Unit]
+Description=OdooManager
+After=network.target
+
+[Service]
+Type=simple
+User=$ODOO_USERNAME
+Group=$ODOO_USERNAME
+ExecStart=$RELEASES_DIR/$ODOOMAN_ODOO_REL/odoo-bin --database=odooman --db-filter="odooman" --config $ODOOMAN_DIR/configs/odooman.conf --logfile /odoo/logs/odoomanager.log
+KillMode=mixed
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+######################################
+# Setup PostgreSQL:
+######################################
+# Create the user:
+sudo -u postgres bash -c "createuser -s $ODOO_USERNAME"
+# Set the password:
+
 
 ######################################
 # Fix permissions:
@@ -77,3 +161,7 @@ sudo find "$ODOOMAN_DIR"/ -type d -exec chmod ug+x {} \;
 sudo usermod -aG "$ODOO_USERNAME" "$USER"
 
 unset ODOO_PASSWORD
+
+# Known Caveats:
+echo "Error: psycopg2.errors.InvalidParameterValue: new encoding (UTF8) is incompatible with the encoding of the template database (SQL_ASCII)"
+echo "https://stackoverflow.com/questions/16736891/pgerror-error-new-encoding-utf8-is-incompatible"
