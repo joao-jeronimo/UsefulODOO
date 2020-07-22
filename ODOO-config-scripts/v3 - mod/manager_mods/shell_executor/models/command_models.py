117d6d3e1110a85f63224d29ed917bb4f98c3b1f
Joao Jeronimo <joao.jeronimo.pro@gmail.com>
2020-07-22 00:11:48 +0000

    Ja da para escolher os argumentos...

--- ODOO-config-scripts/v3 - mod/manager_mods/shell_executor/models/command_models.py
+++ ODOO-config-scripts/v3 - mod/manager_mods/shell_executor/models/command_models.py
@@ -23,17 +23,14 @@ class ExecutorCommand(models.Model):
     name = fields.Char(string="Name")
     
     bash_code = fields.Text(string="Bash code to execute")
-    def execute(self, **kwargs):
+    def execute(self, locals_dict):
         self.ensure_one()
-        locals_dict = dict()
-        for key, value in kwargs.items():
-            locals_dict[key] = velue
         # Substitute arguments:
         real_bash_code = self.bash_code%locals_dict
         # Execute the code on local host:
         bash_result = proclib.run_into_string(['/bin/bash', '-c', real_bash_code])
         # Save the resulting text:
-        self.bash_result = bash_result
+        return bash_result
     
     bash_result = fields.Text(string="Command result")
     
@@ -41,4 +38,44 @@ class ExecutorCommand(models.Model):
     ### Buttons: ########
     #####################
     def button_execute(self):
-        self.execute()
+        self.ensure_one()
+        self.bash_result = self.execute()
+
+class ExecutorCommandCall(models.Model):
+    _name = 'access_server.command.call'
+    
+    base_command = fields.Many2one("access_server.command", string="The base command to execute")
+    arguments_ids = fields.One2many("access_server.command.argument", inverse_name="call_id",
+                                   string="Arguments")
+    bash_result = fields.Text(string="Command result")
+    
+    def name_get(self):
+        res = []
+        for c in self:
+            repres = _("%s") % (self.base_command, )
+            res.append((c.id, repres))
+        return res
+    
+    #####################
+    ### Buttons: ########
+    #####################
+    def button_execute(self):
+        self.ensure_one()
+        locals_dict = dict()
+        for thisArg in self.arguments_ids:
+            locals_dict[thisArg.key] = thisArg.value
+        self.bash_result = self.base_command.execute(locals_dict)
+
+class ExecutorCommandArgument(models.Model):
+    _name = 'access_server.command.argument'
+    
+    call_id = fields.Many2one("access_server.command.call")
+    key = fields.Char(string="Key")
+    value = fields.Text(string="Value of the argument")
+    
+    def name_get(self):
+        res = []
+        for c in self:
+            repres = _("(%s: %s)") % (self.key, self.value, )
+            res.append((c.id, repres))
+        return res
