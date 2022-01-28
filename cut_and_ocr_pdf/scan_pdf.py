e85e374b4d0ce2f4e2f83ce77c9f55e1146b15d6
João Jerónimo <joao.jeronimo.pro@gmail.com>
2022-01-28 01:35:13 +0000

    Scanner rígido.

--- cut_and_ocr_pdf/scan_pdf.py
+++ cut_and_ocr_pdf/scan_pdf.py
@@ -1,10 +1,47 @@
+import re
 
 class PDFPattern:
-    def __init__(self, name, x1, ):
-        pass
+    def __init__(self, name, x1, y1, x2, y2, w, h, detection_policy="firstmatch", pattern_detect=None, pattern_regex=None):
+        (self.name, self.x1, self.y1, self.x2, self.y2, self.w, self.h, self.detection_policy) = (name, x1, y1, x2, y2, w, h, detection_policy)
+        if (pattern_detect is not None and pattern_regex is not None) or (pattern_detect is None and pattern_regex is None):
+            raise BaseException("You need to provide exactly one pattern_detect or a pattern_regex.")
+        if pattern_regex is not None:
+            pattern_detect = (lambda text: re.search(pattern_regex, text, flags=0))
+        self.pattern_detect = pattern_detect
+    
+    def find(scanner):
+        valid_matches = []
+        (xi, yi) = (0, 0)
+        while self.x1+xi+w <= self.x2:
+            while self.y1+yi+h <= self.y2:
+                # Calc coords to detect:
+                detect_x1 = self.x1+xi
+                detect_y1 = self.y1+yi
+                # Try to detect:
+                possible_match = scanner.ocrpdf.extract_text_bybox(1, detect_x1, detect_y1, w, h)
+                if self.pattern_detect(possible_match):
+                    valid_matches += possible_match
+                # Increment y index var:
+                yi += 1
+            # Increment x index var:
+            xi += 1
+        # Return the longest match:
+        if  self.detection_policy == "firstmatch":
+            return valid_matches[0]
+        elif self.detection_policy == "longestmatch":
+            max_match_len = max([len(mat) for mat in valid_matches])
+            return [mat for mat in valid_matches if len(mat)==max_match_len][0]
+        else:
+            raise BaseException("Unknown detection policy %s."%self.detection_policy)
 
 class RigidPDFScanner:
     def __init__(self, ocrpdf):
         self.ocrpdf = ocrpdf
     
+    patterns = {}
     
+    def addPattern(self, pattern):
+        patterns[pattern.name] = patterns
+    
+    def findPattern(self, name):
+        return patterns[name].find(self)
