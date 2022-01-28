64f2e286c490328fce0dc27e5ef62c6b4404b1db
João Jerónimo <joao.jeronimo.pro@gmail.com>
2022-01-28 20:15:56 +0000

    Classe OCRPattern não recebe valores no construtor. Em vez disso terá um conjunto de métodos NotImplementedError, que receberão **kwargs e devolverão esses parâmetros.

--- cut_and_ocr_pdf/scan_pdf.py
+++ cut_and_ocr_pdf/scan_pdf.py
@@ -1,24 +1,38 @@
 import re, pdb
 
 class OCRPattern:
-    def __init__(self, name, x1, y1, x2, y2, w, h, detection_policy="firstmatch", pattern_detect=None, pattern_regex=None):
-        (self.name, self.x1, self.y1, self.x2, self.y2, self.w, self.h, self.detection_policy) = (name, x1, y1, x2, y2, w, h, detection_policy)
+    def __init__(self, name, detection_policy="firstmatch", pattern_detect=None, pattern_regex=None):
+        (self.name, self.detection_policy) = (name, detection_policy)
         if (pattern_detect is not None and pattern_regex is not None) or (pattern_detect is None and pattern_regex is None):
             raise BaseException("You need to provide exactly one pattern_detect or a pattern_regex.")
         if pattern_regex is not None:
             pattern_detect = (lambda text: re.search(pattern_regex, text, flags=0))
         self.pattern_detect = pattern_detect
     
+    #x1, y1, x2, y2, w, h
+    def getX1(self, **kwparams):
+        raise NotImplementedError()
+    def getY1(self, **kwparams):
+        raise NotImplementedError()
+    def getX2(self, **kwparams):
+        raise NotImplementedError()
+    def getY2(self, **kwparams):
+        raise NotImplementedError()
+    def getW(self, **kwparams):
+        raise NotImplementedError()
+    def getH(self, **kwparams):
+        raise NotImplementedError()
+    
     def find(self, scanner):
         valid_matches = []
         (xi, yi) = (0, 0)
-        while self.x1+xi+self.w <= self.x2:
-            while self.y1+yi+self.h <= self.y2:
+        while self.getX1()+xi+self.getW() <= self.getX2():
+            while self.getY1()+yi+self.getH() <= self.getY2():
                 # Calc coords to detect:
-                detect_x1 = self.x1+xi
-                detect_y1 = self.y1+yi
+                detect_x1 = self.getX1()+xi
+                detect_y1 = self.getY1()+yi
                 # Try to detect:
-                possible_match = scanner.ocrpdf.extract_text_bybox(0, detect_x1, detect_y1, self.w, self.h)
+                possible_match = scanner.ocrpdf.extract_text_bybox(0, detect_x1, detect_y1, self.getW(), self.getH())
                 if self.pattern_detect(possible_match):
                     valid_matches.append(possible_match)
                 # Increment y index var:
