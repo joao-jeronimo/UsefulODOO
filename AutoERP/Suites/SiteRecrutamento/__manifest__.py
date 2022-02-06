dcdb30a974cfdf743a7c0669f72b084e3fe9839f
João Jerónimo <joao.jeronimo.pro@gmail.com>
2022-02-06 16:20:54 +0000

    Nova suite para recrutamento.

---
+++ AutoERP/Suites/SiteRecrutamento/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name':         "O Site de recrutamento baseado em Odoo15",
+    'odoo_branch':  "15.0",
+    'repositories': [
+        ],
+    'modules':  [
+        { 'name': 'website',   'active': True, },
+        ],
+    }
