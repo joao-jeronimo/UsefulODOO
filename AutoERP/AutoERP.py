4dda9d68a6d607d67d90b8f33c2ff00ffb0fddd4
João Jerónimo <joao.jeronimo.pro@gmail.com>
2022-03-08 00:42:36 +0000

    Passada grande parte da l´gica oara a classe da instância.

--- AutoERP/AutoERP.py
+++ AutoERP/AutoERP.py
@@ -52,120 +52,26 @@ def suite_info(suitename):
     suite = SuiteTemplate(suitename)
     suite.suite_info()
 
-RELEASE_PYVER = {
-    '13.0':         "3.7",
-    }
+
 
 @opermode
 def create_instance(release_num, instancenm, httpport, private):
     """
     ** Pass 1  - Call this first, then fetch_suite_repos
     """
-    if release_num in RELEASE_PYVER.keys(): python_version = RELEASE_PYVER[release_num]
-    else:                                   python_version = "3.7"
-    # A list of targets to run:
-    TARGETS_TO_RUN = [
-        "prepare_virtualenv",
-        "prepare_all",
-        ]
-    # Prepara parameters:
-    makefile_params = dict(
-        instancenm              = instancenm,
-        httpport                = httpport,
-        listen_on               = "127.0.0.1" if private==1 else "0.0.0.0",
-        odoo_rel                = release_num,
-        wkhtmltopdf_version     = "0.12.6-1",
-        debian_codename         = "buster",
-        python_major_version    = python_version.split('.')[0],
-        python_minor_version    = python_version.split('.')[1],
-        instance_modfolders     = os.path.join(os.path.sep, "odoo", ("custom_%s"%release_num)),
-        pythonlibs_dir          = "/odoo/PythonLibs",
-        targetnames             = " ".join(TARGETS_TO_RUN),
-        )
-    # Run all the needed targets:
-    call_makefile(**makefile_params)
+    inst = autoerp_lib.OdooInstance(instancenm)
+    inst.create_instance(release_num, httpport, private)
 
 @opermode
 def install_suite(release_num, instancenm, httpport, suitename, private):
-    instance_folder = os.path.join(autoerp_lib.INSTANCES_DIR, instancenm)
-    subprocess.check_output(["mkdir", "-p", instance_folder, ])
-    # Spawn the suite and fetch repos:
-    suite = autoerp_lib.SuiteTemplate(suitename)
-    suite.fetch_suite_repos(os.path.join(instance_folder, "SuiteRepos"))
-    # Create the instance:
-    create_instance(release_num, instancenm, httpport, private)
-    # Create instance config folder and file:
-    with open(os.path.join(instance_folder, "instance.conf"), "w") as inst_config_file:
-        inst_config_file.write("suitename = %s" % suitename)
+    inst = autoerp_lib.OdooInstance(instancenm, suitename)
+    inst.install_suite(release_num, httpport, private)
 
 @opermode
-def install_release(release_num):
-    if release_num in RELEASE_PYVER.keys(): python_version = RELEASE_PYVER[release_num]
-    else:                                   python_version = "3.7"
-    # A list of targets to run:
-    TARGETS_TO_RUN = [
-        "/odoo/stages/dep_branch_requirements",
-        "/odoo/stages/dep_wkhtmltopdf",
-        "/odoo/stages/system_user_created",
-        "/odoo/logs",
-        "/odoo/releases/%s"%(release_num),
-        "/odoo/stages/sql_user_created",
-        ]
-    # Prepara parameters:
-    makefile_params = dict(
-        #instancenm              = "",
-        #httpport                = "",
-        #listen_on               = "",
-        odoo_rel                = release_num,
-        wkhtmltopdf_version     = "0.12.6-1",
-        debian_codename         = "buster",
-        python_major_version    = python_version.split('.')[0],
-        python_minor_version    = python_version.split('.')[1],
-        instance_modfolders     = os.path.join(os.path.sep, "odoo", ("custom_%s"%release_num)),
-        pythonlibs_dir          = "/odoo/PythonLibs",
-        targetnames             = " ".join(TARGETS_TO_RUN),
-        )
-    # Run all the needed targets:
-    call_makefile(**makefile_params)
-
-def call_makefile(   odoo_rel="", instancenm="", listen_on="0.0.0.0", httpport="8999",
-                        wkhtmltopdf_version="0.12.6-1", debian_codename="buster", python_major_version="3", python_minor_version="7",
-                        instance_modfolders="", pythonlibs_dir="",
-                        targetnames="prepare_all" ):
-    # Vars dictionary:
-    make_vars = {
-        'ODOO_REL'              : odoo_rel,
-        'INSTANCENM'            : instancenm,
-        'LISTEN_ON'             : listen_on,
-        'HTTPPORT'              : httpport,
-        'WKHTMLTOPDF_VERSION'   : wkhtmltopdf_version,
-        'INSTANCE_MODFOLDERS'   : instance_modfolders,
-        'PYTHON_MAJOR_VERSION'  : python_major_version,
-        'PYTHON_MINOR_VERSION'  : python_minor_version,
-        'PYTHONLIBS_DIR'        : pythonlibs_dir,
-        
-        'DEBIAN_CODENAME'       : debian_codename,
-        'DISTRO'                : "ubuntu",
-        }
-    # Convert dictionary to arguments:
-    make_vars_list = ",".join(make_vars.keys())
-    # Do call the makefile:
-    subprocess.check_output([
-        "sudo",
-        "--preserve-env=%(make_vars_list)s" % { 'make_vars_list':   make_vars_list, },
-        "make", "-f", "Odoo.makefile",
-        *( targetnames.split(" ") ),
-        ],
-        env = make_vars,
-        )
-
-@opermode
-def prepare_virtualenv(python_version="3.7"):
-    call_makefile(
-        python_major_version    = python_version.split('.')[0],
-        python_minor_version    = python_version.split('.')[1],
-        targetnames              = "prepare_virtualenv",
-        )
+def full_launch_instance(release_num, instancenm, httpport, suitename, private):
+    inst = autoerp_lib.OdooInstance(instancenm, suitename)
+    inst.install_suite(release_num, httpport, private)
+    inst.start_instance()
 
 @opermode
 def get_instance_config(instancenm):
--- AutoERP/autoerp_lib.py
+++ AutoERP/autoerp_lib.py
@@ -13,6 +13,10 @@ ODOO_ROOT = os.path.join(os.path.sep, "odoo")
 SUITE_TEMPLATE_DIR = os.path.join(os.path.curdir, "Suites")
 INSTANCES_DIR = os.path.join(ODOO_ROOT, "Instances")
 
+RELEASE_PYVER = {
+    '13.0':         "3.7",
+    }
+
 class SuiteTemplate:
     def __init__(self, suitename):
         (
@@ -66,12 +70,12 @@ class SuiteTemplate:
                 )
 
 class OdooInstance:
-    def __init__(self, instacename, suitename=None):
+    def __init__(self, instancename, suitename=None):
         (
-            self.instacename,
+            self.instancename,
             self.suitename,
             ) = (
-                instacename,
+                instancename,
                 suitename,
             )
     
@@ -81,7 +85,7 @@ class OdooInstance:
             os.path.sep,
             'odoo',
             'configs',
-            'odoo-%s.conf' % (self.instacename,),
+            'odoo-%s.conf' % (self.instancename,),
             )
     
     ### Parsing files:
@@ -102,10 +106,87 @@ class OdooInstance:
             'hostname'  : 'localhost',
             'port'      : self.get_http_port(),
             'protocol'  : 'jsonrpc',
-            'database'  : self.instacename,
+            'database'  : self.instancename,
             'login'     : username,
             'user_id'   : user_id,
             }
     
     def get_communicator(self, username="admin", user_id=2):
         return util_odoo.OdooCommunicator(self.get_odoo_connection_params(username, user_id))
+    
+    #################################################################################
+    ### Bootstrap & Startup logic: ##################################################
+    #################################################################################
+    def _call_makefile(self, odoo_rel="", instancenm="", listen_on="0.0.0.0", httpport="8999",
+                            wkhtmltopdf_version="0.12.6-1", debian_codename="buster", python_major_version="3", python_minor_version="7",
+                            instance_modfolders="", pythonlibs_dir="",
+                            targetnames="prepare_all" ):
+        # Vars dictionary:
+        make_vars = {
+            'ODOO_REL'              : odoo_rel,
+            'INSTANCENM'            : instancenm,
+            'LISTEN_ON'             : listen_on,
+            'HTTPPORT'              : httpport,
+            'WKHTMLTOPDF_VERSION'   : wkhtmltopdf_version,
+            'INSTANCE_MODFOLDERS'   : instance_modfolders,
+            'PYTHON_MAJOR_VERSION'  : python_major_version,
+            'PYTHON_MINOR_VERSION'  : python_minor_version,
+            'PYTHONLIBS_DIR'        : pythonlibs_dir,
+            
+            'DEBIAN_CODENAME'       : debian_codename,
+            'DISTRO'                : "ubuntu",
+            }
+        # Convert dictionary to arguments:
+        make_vars_list = ",".join(make_vars.keys())
+        # Do call the makefile:
+        subprocess.check_output([
+            "sudo",
+            "--preserve-env=%(make_vars_list)s" % { 'make_vars_list':   make_vars_list, },
+            "make", "-f", "Odoo.makefile",
+            *( targetnames.split(" ") ),
+            ],
+            env = make_vars,
+            )
+    
+    def create_instance(self, release_num, httpport, private):
+        """
+        ** Pass 1  - Call this first, then fetch_suite_repos
+        """
+        if release_num in RELEASE_PYVER.keys(): python_version = RELEASE_PYVER[release_num]
+        else:                                   python_version = "3.7"
+        # A list of targets to run:
+        TARGETS_TO_RUN = [
+            "prepare_virtualenv",
+            "prepare_all",
+            ]
+        # Prepara parameters:
+        makefile_params = dict(
+            instancenm              = self.instancename,
+            httpport                = httpport,
+            listen_on               = "127.0.0.1" if private==1 else "0.0.0.0",
+            odoo_rel                = release_num,
+            wkhtmltopdf_version     = "0.12.6-1",
+            debian_codename         = "buster",
+            python_major_version    = python_version.split('.')[0],
+            python_minor_version    = python_version.split('.')[1],
+            instance_modfolders     = os.path.join(os.path.sep, "odoo", ("custom_%s"%release_num)),
+            pythonlibs_dir          = "/odoo/PythonLibs",
+            targetnames             = " ".join(TARGETS_TO_RUN),
+            )
+        # Run all the needed targets:
+        self._call_makefile(**makefile_params)
+    
+    def install_suite(self, release_num, httpport, private):
+        instance_folder = os.path.join(autoerp_lib.INSTANCES_DIR, self.instancename)
+        subprocess.check_output(["mkdir", "-p", instance_folder, ])
+        # Spawn the suite and fetch repos:
+        suite = autoerp_lib.SuiteTemplate(self.suitename)
+        suite.fetch_suite_repos(os.path.join(instance_folder, "SuiteRepos"))
+        # Create the instance:
+        self.create_instance(release_num, httpport, private)
+        # Create instance config folder and file:
+        with open(os.path.join(instance_folder, "instance.conf"), "w") as inst_config_file:
+            inst_config_file.write("suitename = %s" % self.suitename)
+    
+    def start_instance(self):
+        subprocess.check_output(['sudo', 'systemctl', 'start', 'odoo-%s'%self.instancename])
