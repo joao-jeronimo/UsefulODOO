3cde9c64de6d5b4be12e97f98f732f99a0f3709e
João Jerónimo <joao.jeronimo.pro@gmail.com>
2022-06-24 08:17:32 +0000

    Corrigidos erros de sintaxe.

--- AutoERP/Suites/ElanceoDevel/IncludedApps/14.0/rich_payslip_editing/__manifest__.py
+++ AutoERP/Suites/ElanceoDevel/IncludedApps/14.0/rich_payslip_editing/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Rich Payslip Editing - A fork of payslip_review module',
-    'description'   : 'A fork of https://apps.odoo.com/apps/modules/12.0/payslip_review/'
-    'version': '14.0.01',
+    'description'   : 'A fork of https://apps.odoo.com/apps/modules/12.0/payslip_review/',
+    'version': '14.0.1',
     'author': 'João Jerónimo',
     'category': 'Payroll',
     'data': [
