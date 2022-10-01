44dc88bd56113bd751612adf2bba6c4e55fa749a
João Jerónimo <joao.jeronimo.pro@gmail.com>
2022-10-01 20:04:57 +0000

    Samples OCR. MAndar instalar o novo módulo de anexos dinâmicos.

--- AutoERP/Suites/Odoo-OCR-Framework/__init__.py
+++ AutoERP/Suites/Odoo-OCR-Framework/__init__.py
@@ -23,6 +23,7 @@ def port_app(basedir, app_name_snakecase, target_name):
     pporter.do_preprocess_directory()
 
 APPS_TO_PORT = [
+    { 'name': "dynamic_attachments",                      'exclude_rels': [] },
     { 'name': "tesseract_ocr_wrapper",                      'exclude_rels': [] },
     ]
 
--- AutoERP/Suites/Odoo-OCR-Framework/__manifest__.py
+++ AutoERP/Suites/Odoo-OCR-Framework/__manifest__.py
@@ -31,6 +31,7 @@
         ],
     'modules':  [
         # Base apps:
+        { 'name': 'dynamic_attachments',                  'active': ["15.0", ], },
         { 'name': 'tesseract_ocr_wrapper',                  'active': ["15.0", ], },
         ],
 }
