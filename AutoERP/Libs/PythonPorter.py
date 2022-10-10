bfe7661aca0b96015293060a45c1b509d252f23f
João Jerónimo <joao.jeronimo.pro@gmail.com>
2022-10-10 00:20:20 +0000

    Heavy refactorization.

--- AutoERP/Libs/PythonPorter.py
+++ AutoERP/Libs/PythonPorter.py
@@ -1,7 +1,7 @@
 import os, re, pdb, subprocess
 from io import StringIO
 
-class BinaryFileException(BaseException):
+class BinaryFileException(Exception):
     pass
 
 class PythonPorter:
@@ -13,6 +13,9 @@ class PythonPorter:
         self.destdir        = destdir
         self.filename_regex = re.compile(filename_regex, flags=0)
     
+    #############################################
+    ##### Loading and applying macroes: #########
+    #############################################
     def load_macros(self):
         # Load list of macros:
         all_macro_keys = [
@@ -29,13 +32,13 @@ class PythonPorter:
             self.macroes_dikt[this_macro_key] = this_macro_value
             macrofileh.close()
     
-    def apply_macroes_to_file(self, filename):
-        if not self.filename_regex.search(filename):
-            raise BinaryFileException()
-        srcfile = open(filename, "r")
-        srctxt = srcfile.read()
-        srcfile.close()
-        # Apply all macroes - many time until no replacements rests:
+    def apply_macroes_to_string(self, srctxt):
+        """
+        Applies every macro to the passed string.
+        Every macro is applied in order. After last macro is applied, if macro expansion was
+        performed at least one time, then the macro expansion is started over again. The
+        process is repeated until there is one round without any expansion.
+        """
         dsttxt = srctxt
         while True:
             n_replacements = 0
@@ -48,7 +51,29 @@ class PythonPorter:
                 break
         return dsttxt
     
+    def apply_macroes_to_file(self, filename):
+        """
+        Reads a file and applies every macro to it.
+        """
+        # Passed filename must fall within macro expansion scope
+        # regex. If not, raise exception:
+        if not self.filename_regex.search(filename):
+            raise BinaryFileException()
+        # Read source file:
+        with open(filename, "r") as srcfile:
+            srctxt = srcfile.read()
+        # Apply all macroes - many time until no replacements rests:
+        return self.apply_macroes_to_string(srctxt)
+    
+    ###################################################################################
+    ##### Finding and filtering files ad folders to run the preprocessor on:    #######
+    ###################################################################################
     def preprocess_find_output(self, find_output):
+        """
+        Transforms the absolute paths returned by Unix "find" into paths that
+        are relative to self.sourcedir.
+        TODO: This does not currently tolerate dirnames or filenames with spaces!
+        """
         haldir = [
             re.sub("^"+self.sourcedir+"/*", "", dirpath)
             for dirpath in find_output.strip().split()
@@ -56,44 +81,85 @@ class PythonPorter:
         return haldir
     
     def find_dirs(self):
-        find_output = subprocess.check_output([ "find", self.sourcedir, "-mindepth", "1", "-type", "d" ])
-        #, in_stream=StringIO(postgres_input)
+        """
+        Find every subdir of self.sourcedir (does not include self.sourcedir
+        itself in the list). Returns a list of relative paths.
+        """
+        find_output = subprocess.check_output([
+            "find", self.sourcedir,
+            "-mindepth", "1",
+            "-type", "d",
+            ])
         haldir = self.preprocess_find_output(find_output.decode())
         return haldir
-    
     def find_files(self):
-        find_output = subprocess.check_output([ "find", self.sourcedir, "-mindepth", "1", "-type", "f" ])
-        #, in_stream=StringIO(postgres_input)
+        """
+        The same as find_dirs(), but returns file paths instead of subdirectory paths.
+        """
+        find_output = subprocess.check_output([
+            "find", self.sourcedir,
+            "-mindepth", "1",
+            "-type", "f",
+            ])
         halfile = self.preprocess_find_output(find_output.decode())
         return halfile
     
     def filter_matching(self, fileslist):
         halmatching = [ dirpath for dirpath in fileslist if self.filename_regex.search(dirpath) ]
         return halmatching
-    
     def filter_non_matching(self, fileslist):
         halnotmatching = [ dirpath for dirpath in fileslist if not self.filename_regex.search(dirpath) ]
         return halnotmatching
     
-    def copy_dir_structure(self):
-        haldirs = self.find_dirs()
-        subprocess.check_output(["mkdir", "-p", self.destdir])
-        for thisdir in haldirs:
-            thisdir_relative = re.sub("^"+self.sourcedir+"/*", "", thisdir)
-            subprocess.check_output(["sudo", "rm", "-rf", os.path.join(self.destdir, thisdir_relative)])
-        for thisdir in haldirs:
-            thisdir_relative = re.sub("^"+self.sourcedir+"/*", "", thisdir)
-            subprocess.check_output([ "mkdir", os.path.join(self.destdir, thisdir_relative) ])
-    
-    def transfer_file(self, filename):
+    def compute_absolute_paths(self, filename):
+        """
+        Compute source and destination absolute paths for filename.
+        """
         real_src_path = os.path.join(self.sourcedir, filename)
         real_dst_path = os.path.join(self.destdir, filename)
+        return (real_src_path, real_dst_path)
+    
+    ################################################
+    ##### Cloning directory structures:      #######
+    ################################################
+    def copy_dir_structure(self):
+        # Find every relative paths of the packet to port:
+        alldirs_relative = self.find_dirs()
+        # Make the root dir of the final destination, if it does not already exist:
+        subprocess.check_output(["mkdir", "-p", self.destdir])
+        # TODO: macro-substitute every directory to create:
+        destination_dirs_relative = alldirs_relative
+        # Build full destination paths:
+        destination_dirs_absolute = [
+            os.path.join(self.destdir, thisdir_relative)
+            for thisdir_relative in destination_dirs_relative
+            ]
+        # Remove original directories at the destination:
+        for thisdir_absolute in destination_dirs_absolute:
+            subprocess.check_output([
+                "sudo", "rm", "-rf", thisdir_absolute,
+                ])
+        # Create new destinations directories:
+        for thisdir_absolute in destination_dirs_absolute:
+            subprocess.check_output([
+                "mkdir", thisdir_absolute,
+                ])
+    
+    ########################################################################################
+    ##### Individual file processing - both with macro expsansion and without it:    #######
+    ########################################################################################
+    def transfer_file(self, filename):
+        """
+        Call this for every file that shall NOT undergo macro substitution.
+        """
+        (real_src_path, real_dst_path) = self.compute_absolute_paths(filename)
         subprocess.check_output([ "cp", real_src_path, real_dst_path ])
     
     def preproccess_and_transfer_file(self, filename):
-        real_src_path = os.path.join(self.sourcedir, filename)
-        real_dst_path = os.path.join(self.destdir, filename)
-        #pdb.set_trace()
+        """
+        Call this for every file that must undergo macro substitution.
+        """
+        (real_src_path, real_dst_path) = self.compute_absolute_paths(filename)
         try:
             proproc_out = self.apply_macroes_to_file(real_src_path)
         except UnicodeDecodeError:
