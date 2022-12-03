408bd0c93305b9f81ddb4091b46df38becbb5e38
João Jerónimo <joao.jeronimo.pro@gmail.com>
2022-12-03 19:29:25 +0000

    Uma suita para instalar a parte dos backups.

---
+++ AutoERP/Suites/Odoo-JJ-System-Modules/__init__.py
@@ -0,0 +1,31 @@
+import os
+from PythonPorter import PythonPorter
+
+def port_app(basedir, app_name_camelcase, target_name):
+    print("Porting app %s to Odoo %s" % (app_name_camelcase, target_name,))
+    format_names = {
+        'basedir'               : basedir,
+        'app_name_camelcase'    : app_name_camelcase,
+        'target_name'           : target_name,
+        }
+    # Params:
+    macrodir = "%(basedir)s/%(app_name_camelcase)s/Targets/%(target_name)s" % format_names
+    srcdir   = "%(basedir)s/%(app_name_camelcase)s/Src"                     % format_names
+    dstdir   = "%(basedir)s/0_Installable/%(target_name)s"                  % format_names
+    # Run porter for app and release:
+    pporter = PythonPorter(macrodir, srcdir, dstdir)
+    pporter.load_macros()
+    pporter.do_preprocess_directory()
+
+APPS_TO_PORT = [
+    { 'name': "ExtensibleAutoBackup",                   'exclude_rels': [] },
+    ]
+
+def port_modules_odoo(instance, repo_localname, basedir):
+    # Find out Odoo release of the instance:
+    odoo_release = instance.release_num
+    repo_dir = os.path.join(basedir, repo_localname)
+    # Port each app:
+    for app in APPS_TO_PORT:
+        if odoo_release not in app['exclude_rels']:
+            port_app(repo_dir, app['name'], odoo_release)
---
+++ AutoERP/Suites/Odoo-JJ-System-Modules/__manifest__.py
@@ -0,0 +1,38 @@
+{
+    'name':         "A set of system modules do Odoo",
+    'repositories': [
+        #{
+        #    'type':             "included",
+        #    'srcpath':          "./Apps/Payroll/%(odoo_rel)s",
+        #    'localname':        "Payroll",
+        #    'odoo_releases':    ["13.0", "14.0", "15.0", ],
+        #    'modpaths': [
+        #        ".",
+        #        ],
+        #    },
+        {
+            'type':             "git",
+            'url':              "git@github.com:joao-jeronimo/Odoo-System-mods.git",
+            'localname':        "Odoo-System-mods",
+            'branch':           "main",
+            'odoo_releases':    [
+                #"11.0",
+                #"12.0",
+                #"13.0",
+                "14.0",
+                #"15.0",
+                ],
+            # Python function to port the modules after clone/checkout.
+            'post_fetch_hook':  "port_modules_odoo",
+            'modpaths': [
+                "0_Installable/%(odoo_rel)s",
+                ],
+            },
+        ],
+    'modules':  [
+        # Base apps:
+        { 'name': 'extensible_auto_backup',                 'active': ["15.0", ], },
+        { 'name': 'extensible_auto_backup_googledrive',     'active': ["15.0", ], },
+        #{ 'name': 'extensible_auto_backup_sftp',            'active': ["15.0", ], },
+        ],
+}
